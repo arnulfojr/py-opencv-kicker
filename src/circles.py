c1c3a3829271d177b325cdbd15ad105e42c63094
Arnulfo Solis <arnulfojr94@gmail.com>
2018-04-14 17:52:07 +0200

    circle detection

--- src/circles.py
+++ src/circles.py
@@ -1,46 +1,59 @@
 import cv2
 import numpy as np
 import sys
+import csv
 
 # Read video
-video = cv2.VideoCapture(0)
+video = cv2.VideoCapture('game2.mov')
 
 # Exit if video not opened.
 if not video.isOpened():
     print("Could not open video")
     sys.exit()
 
-while True:
-    # Read first frame.
-    ok, image = video.read()
-    if not ok:
-        print('Cannot read video file')
-        sys.exit()
+counter = 0
 
-    output = image
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+with open('results.csv', 'x') as csv_file:
+    csv_writer = csv.writer(csv_file, delimiter=' ',
+                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    while True:
+        # Read first frame.
+        ok, image = video.read()
+        counter = counter + 1
+        if not ok:
+            print('Cannot read video file')
+            sys.exit()
 
-    # detect circles in the image
-    # 24-30 min-max
-    circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, 200,
-                               param1=50, param2=20, minRadius=24, maxRadius=30)
+        output = image
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        blur = cv2.bilateralFilter(gray, 7, 75, 75)
 
-    # ensure at least some circles were found
-    if circles is not None:
-        # convert the (x, y) coordinates and radius of the circles to integers
-        circles = np.round(circles[0, :]).astype("int")
+        # detect circles in the image
+        # 24-30 min-max
+        circles = cv2.HoughCircles(blur, cv2.HOUGH_GRADIENT, 1, 200,
+                                   param1=50, param2=20, minRadius=12, maxRadius=18)
 
-        # loop over the (x, y) coordinates and radius of the circles
-        for (x, y, r) in circles:
-            # draw the circle in the output image, then draw a rectangle
-            # corresponding to the center of the circle
-            cv2.circle(output, (x, y), r, (0, 255, 0), 4)
-            cv2.rectangle(output, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
-    # show the output image
-    cv2.imshow("output", output)
+        # ensure at least some circles were found
+        if circles is not None:
+            # convert the (x, y) coordinates and radius of the circles to integers
+            circles = np.round(circles[0, :]).astype("int")
 
-    # Exit if ESC pressed
-    k = cv2.waitKey(1) & 0xff
-    if k == 27: break
+            # loop over the (x, y) coordinates and radius of the circles
+            for (x, y, r) in circles:
+                if r > 0:
+                    # draw the circle in the output image, then draw a rectangle
+                    # corresponding to the center of the circle
+                    cv2.circle(blur, (x, y), r, (0, 255, 0), 4)
+                    print(counter, x, y, r)
+                    csv_writer.writerow([counter, x, y, r])
+                    cv2.rectangle(blur, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
+        # show the output image
+        cv2.imshow("output", blur)
 
+        # Exit if ESC pressed
+        k = cv2.waitKey(1) & 0xff
+        if k == 27:
+            break
+
+video.release()
 cv2.destroyAllWindows()
