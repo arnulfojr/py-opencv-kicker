bb74c0375e92e449fab12a08b9deb8a62a7ba78b
Arnulfo Solis <arnulfojr94@gmail.com>
2018-04-15 15:42:50 +0200

    minor code styling

--- src/circles.py
+++ src/circles.py
@@ -43,12 +43,12 @@ def detect_box(output):
         # centroid
         for c in cnts:
             ((x, y), radius) = cv2.minEnclosingCircle(c)
-            M = cv2.moments(c)
-            possible_center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
 
             # only proceed if the radius meets a minimum size
             if CALIBRATION_BOX_MIN_RADIUS < radius < CALIBRATION_BOX_MAX_RADIUS:
-                center = possible_center
+                M = cv2.moments(c)
+                center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+
                 # draw the circle and centroid on the frame,
                 # then update the list of tracked points
                 cv2.circle(output, (int(x), int(y)), int(radius), (0, 255, 255), 2)
@@ -56,7 +56,8 @@ def detect_box(output):
                 cv2.circle(output, center, 5, (0, 0, 255), -1)
 
     if len(pp) > 3:
-        cent = (sum([p[0] for p in pp]) / len(pp), sum([p[1] for p in pp]) / len(pp))
+        cent = (sum([p[0] for p in pp]) / len(pp),
+                sum([p[1] for p in pp]) / len(pp))
         # sort by polar angle
         pp.sort(key=lambda p: math.atan2(p[1] - cent[1], p[0] - cent[0]))
 
@@ -66,7 +67,8 @@ def detect_box(output):
         cv2.line(output, pp[3], pp[0], (255, 0, 0), 5)
     else:
         # Display error message
-        cv2.putText(output, "No contour detected", (100, 150), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50, 170, 50), 2)
+        cv2.putText(output, "No contour detected", (100, 150),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50, 170, 50), 2)
 
 
 def add_circles(frame, detected_circles, output):
@@ -82,7 +84,10 @@ def add_circles(frame, detected_circles, output):
         # draw the circle in the output image, then draw a rectangle
         # corresponding to the center of the circle
         cv2.circle(output, (x, y), r, (0, 255, 0), 4)
-        cv2.rectangle(output, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
+        cv2.rectangle(output,
+                      (x - 5, y - 5),
+                      (x + 5, y + 5),
+                      (0, 128, 255), -1)
 
     # loop over the (x, y) coordinates and radius of the circles
     [draw_circle_in_image(x, y, r) for (x, y, r) in detected_circles
@@ -107,7 +112,8 @@ while True:
     # detect circles in the image
     # 24-30 min-max
     circles = cv2.HoughCircles(blur, cv2.HOUGH_GRADIENT, 1, 200,
-                               param1=50, param2=20, minRadius=BALL_MIN_RADIUS, maxRadius=BALL_MAX_RADIUS)
+                               param1=50, param2=20, minRadius=BALL_MIN_RADIUS,
+                               maxRadius=BALL_MAX_RADIUS)
 
     # detect the outer box based on color points
     detect_box(output)
@@ -118,7 +124,8 @@ while True:
     # Calculate Frames per second (FPS)
     fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer)
     # Display FPS on frame
-    cv2.putText(output, "FPS : " + str(int(fps)), (100, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50, 170, 50), 2)
+    cv2.putText(output, "FPS : " + str(int(fps)), (100, 50),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50, 170, 50), 2)
     # show the output image
     cv2.imshow("output", output)
 
